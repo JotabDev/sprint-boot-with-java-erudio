bc21fba6a90205468a6bbf52cd785b9958334707
jotaBDev <joaobfreire1999@outlook.com>
2025-01-26 14:06:08 -0300

    First EndPointer Test

---
+++ src/main/java/com/br/jotab/SpringBootWithJavaErudioApplication.java
@@ -0,0 +1,13 @@
+package com.br.jotab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithJavaErudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithJavaErudioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/jotab/SpringBootWithJavaErudioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.jotab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithJavaErudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
